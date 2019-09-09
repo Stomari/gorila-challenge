f909ef6200cb9f0bbbd91d68f9b5bd092d2b2cb5
Sergio Festa <sfestatomari@gmail.com>
2019-09-09 20:48:55 -0300

    added styling

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+    <header>
+      <a href="https://gorilainvest.com.br"><img className="logo" src="./images/Icon_Gorila_hiRes.png" alt="Gorila Invest Icon" /></a>
+    </header>
+  )
+}
+
+export default Header;
