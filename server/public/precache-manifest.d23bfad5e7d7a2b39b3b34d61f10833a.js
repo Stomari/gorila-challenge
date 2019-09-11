5b803789ea3cb32e6481cc79c66a1ee384b9ee81
Sergio Festa <sfestatomari@gmail.com>
2019-09-11 17:55:09 -0300

    added docker-compose to server and client

---
+++ server/public/precache-manifest.d23bfad5e7d7a2b39b3b34d61f10833a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cec6500fd4cc469f0eebc647a63cbfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96a1b5926fd8552da799",
+    "url": "/static/css/main.20940935.chunk.css"
+  },
+  {
+    "revision": "20b465eaed5d0626dc3a",
+    "url": "/static/js/2.1a1c3c2f.chunk.js"
+  },
+  {
+    "revision": "96a1b5926fd8552da799",
+    "url": "/static/js/main.e39692ec.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
